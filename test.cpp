2024e85709dce7f3fbd4ea15fe933228c5214f41
ahmadhunain95 <58005014+ahmadhunain95@users.noreply.github.com>
2019-11-20 23:21:15 +0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,27 @@
+// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int i;
+	i = 0;  //ider ruk gia ab agay manually chlao.
+	int j = i;
+	cout << "Hello World!\n";
+}  //Now fine.ok
+//
+//ye breakpoint ha.  is pr aa k program ruk jata ha.
+//Always add it to Git.  ye code he history save krta ha.
+//Like this. bta raha ha kia change kia kia delete or modify.ok
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+//to add code to github
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
